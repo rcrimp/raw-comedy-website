6556fc9f0e7a5107097709d0490a5f84c3384c40
Reuben Crimp <Reuben.Crimp@irisdata.co.nz>
2021-06-11 06:27:00 +1200

    Revert "installed google analytics"
    This reverts commit d645522858040d7e0d54d0429bd8d51d32f55909.

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,6 @@ import {
 } from "react-router-dom";
 
 import HomePage from './components/HomePage'
-import ReactGA from 'react-ga';
-import trackData from './track-data.json';
-
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    console.log('Hello developer');
-} else {
-	console.log('Looking for bugs in my code are ya?');
-	console.log('Are you a software tester?');
-	console.log("If you are, I bet you have heaps of children.");
-	console.log('As you and I both know, software testers are professionally trained to reproduce mistakes.');
-    ReactGA.initialize(trackData.trackingID, {
-		// debug: true,
-		titleCase: false,
-		gaOptions: {
-			userId: "developer",
-			siteSpeedSampleRate: 100,
-			alwaysSendReferrer: true,
-		}
-	  });
-}
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
 
