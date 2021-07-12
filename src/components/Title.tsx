11569efa4200092a24cf9a694091b12c452ea8d8
Reuben Crimp <reubencrimp@gmail.com>
2021-07-12 22:48:17 +1200

    another ONE!

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 // import useMediaQuery from "@material-ui/core/useMediaQuery";
 // import YouTube from 'react-youtube';
 import Vimeo from '@u-wave/react-vimeo';
